573a61cf6f81c699704cee805748e8040861612e
udayjainn <66412347+udayjainn@users.noreply.github.com>
2020-09-26 21:18:12 +0530

    Add files via upload

---
+++ C++/Errors/vectors.cpp
@@ -0,0 +1,22 @@
+//
+//  vectors.cpp
+//  iste
+//
+//  Created by Apple on 26/09/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    //basic functions of vectors
+    //find and correct syntax of which function is wrong
+    vector<int>v;
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+    v[1]=100;
+    v.pop();
+}
